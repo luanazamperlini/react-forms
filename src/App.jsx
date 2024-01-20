4c4f0686f687d8bc8fc903a7ff21014e2df43a3a
luana_zamperlini <luazamperlini@gmail.com>
2024-01-20 12:37:19 -0600

    created component forms an d imported the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-import './App.css'
+import { useState } from 'react';
+import Authenticate from "./components/authenticate";
+import SignUpForm from "./components/signupform";
+
+export default function App(){
+    return(
+        <>
+        <Authenticate />
+        <SignUpForm />
+        </>
+    );
+}
+
+
+
+
 
 
