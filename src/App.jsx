dab6646601bf5a52e6482c66766ecf9860d6698a
luana_zamperlini <luazamperlini@gmail.com>
2024-01-22 12:55:06 -0600

    form-logic signup component in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Authenticate from "./components/authenticate";
 import SignUpForm from "./components/signupform";
 
--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,5 +1,42 @@
 export default function SignUpForm(){
-return <h2>Sign Up!</h2>;
+    const[username, setUsername] = React.useState(" ");
+    const[password, setPassword] = React.useState(" ");
+    const[error, setError] = React.useState(null);
+
+    function handleUsername(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePassword(event){
+        setPassword(event.target.value)
+    }
+
+    function handleError(event){
+        setError(event.target.value)
+    }
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Hello 👋");
+      }
+
+    
+render()
+
+return(
+<>
+
+<h2>Sign Up!</h2>;
+    <form>
+        <label>
+            Username: 
+            <input value = {username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+            Password:
+            <input value = {password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button>Submit</button>
+    </form>
+</>
+)
 }
-
-
